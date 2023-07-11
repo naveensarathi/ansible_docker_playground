733586a51d3e6543b6cb686a8c5e23ab7c9de615
naveensarathi <naveensarathi@gmail.com>
2023-07-11 12:06:29 +0800

    Update ansible_docker_playground_ready.sh

--- ansible_docker_playground_ready.sh
+++ ansible_docker_playground_ready.sh
@@ -1,4 +1,9 @@
+echo "1) Installing Servers"
 docker run -it -d mmumshad/ubuntu-ssh-enabled ;docker run -it -d mmumshad/ubuntu-ssh-enabled ;docker run -it -d mmumshad/ubuntu-ssh-enabled 
+
+echo "2) Installing Ansible"
+pip install ansible 
+echo "3) Creating inventory.txt" 
 y=1
 for x in `docker ps | awk '{print $1}' | grep -v CONTAINER` 
 do
@@ -6,4 +11,6 @@ ip=`docker inspect $x | grep -i IPAddress | grep -v Sec| cut -f2 -d':' | uniq | 
 echo "target$y ansible_host=$ip ansible_ssh_pass=Passw0rd" >> inventory.txt
 y=`echo $y+1 | bc `
 done
+
+
 cat inventory.txt 
