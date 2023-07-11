6a557e185cd99d57452c1111db5f4dff73664f7a
naveensarathi <naveensarathi@gmail.com>
2023-07-11 13:19:24 +0800

    Update ansible_docker_playground_ready.sh

--- ansible_docker_playground_ready.sh
+++ ansible_docker_playground_ready.sh
@@ -9,7 +9,7 @@ y=1
 for x in `docker ps | awk '{print $1}' | grep -v CONTAINER` 
 do
 ip=`docker inspect $x | grep -i IPAddress | grep -v Sec| cut -f2 -d':' | uniq | tr "\"" ' ' | tr "," ' '| awk '{print $1}'`
-echo "target$y ansible_host=$ip ansible_ssh_pass=Passw0rd ansible_ssh_common_args='StrictHostKeyChecking=no'" >> inventory.txt
+echo "target$y ansible_host=$ip ansible_ssh_pass=Passw0rd ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> inventory.txt
 y=`echo $y+1 | bc `
 done
 cat inventory.txt 
